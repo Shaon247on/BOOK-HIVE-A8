9adc699de8ad49b2a31aa33e36d1ee8991f29a6c
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-03-27 05:02:11 +0600

    time to sleep

--- src/Components/ReadBooks/ReadBooks.jsx
+++ src/Components/ReadBooks/ReadBooks.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getStoredItem } from "../../Utlitity/localStorage";
 import { useLoaderData } from "react-router-dom";
 import ReadBook from "../ReadBook/ReadBook";
+import Dropdown from "../DropDown/DropDown";
 
 const ReadBooks = () => {
     const allBooks = useLoaderData()
@@ -19,18 +20,31 @@ const ReadBooks = () => {
             }
         }
     }, [])
-    console.log(reads)
+    const rearrange = [...reads]
+    const [sortedBooks, setSortedBooks] = useState(rearrange)
+    const sortBooksByRating = (reads) => {
+        return [...reads].sort((a, b) => a.rating - b.rating);
+    }
+    const handleSortButtonClick = () => {
+        const sorted = sortBooksByRating(reads);
+        setSortedBooks(sorted);
+    }
     return (
         <div>
             <div className="text-start lg:text-center">
+                <button onClick={handleSortButtonClick}>Try Me</button>
                 <h1 className="text-3xl lg:text-4xl font-bold">Total Read Books: {reads.length}</h1>
             </div>
             <div>
                 {
-                    reads.map(read => <ReadBook key={read.id} read={read}></ReadBook>)
+                    sortedBooks.length > 0 && sortedBooks.map(read => <ReadBook key={read.id} read={read}></ReadBook>)
                 }
-            </div>            
-        </div>        
+                {
+                    sortedBooks.length === 0 && reads.map(read => <ReadBook key={read.id} read={read}></ReadBook>)
+                }
+            </div>
+            <Dropdown function={handleSortButtonClick}></Dropdown>
+        </div>
     );
 };
 
